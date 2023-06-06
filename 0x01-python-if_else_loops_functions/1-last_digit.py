0fc5c7d1cf28eacd9df43b6545d0e88df0992383
thorbie010 <jonahemmanuel54@gmail.com>
2023-06-06 14:07:59 +0000

    finding lasts digits of random numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+tmpnum = number 
+
 if number < 0:
     number = -(number)
+
 y = number % 10
+if tmpnum < 0:
+    number = tmpnum
+    y = -(y)
+
 if y > 5:
     print("Last digit of", number, "is", y,"and is greater than 5")
 elif y == 0:
