1920b2db25f6a6213de947b09f2cab9d804d3768
Jonah <jonahemmanuel54@gmail.com>
2023-06-08 14:00:19 -0700

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 import sys
-import hidden_4 as hid
+import hidden_4 as hidden
 
-if __name__ == "__main__":
-    module_names = dir(hid)
+if __name__ != "__main__":
+    exit()
 
-    filtered_names = [name for name in hid if not name.startswith('__')]
-
-    sorted_names = sorted(filtered_names)
-
-    for name in sorted_names:
+for name in dir(hidden):
+    if name[0:2] != "__":
         print(name)
