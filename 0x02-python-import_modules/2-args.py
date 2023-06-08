df9db80ad67ce171916ab8ed5714b16f4c360548
Jonah <jonahemmanuel54@gmail.com>
2023-06-08 12:22:36 -0700

    printing argument passed to a program

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,11 @@ import sys
 
 if __name__ == "__main__":
     total_args = len(sys.argv) - 1
+
     if total_args < 1:
         print("0 arguments.")
-    print(total_args, "arguments:")
-    for i, arg in enumerate(sys.argv[1:], start=1):
-        print("{}: {}".format(i, arg))
+    
+    else:
+        print(total_args, "arguments:")
+        for i, arg in enumerate(sys.argv[1:], start=1):
+            print("{}: {}".format(i, arg))
