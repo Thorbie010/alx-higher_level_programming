81d4700cae63fbc648791960ea74efba24d7544f
Jonah <jonahemmanuel54@gmail.com>
2023-06-26 05:05:45 -0700

    fix on task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,7 +9,7 @@ def safe_print_list(my_list=[], x=0):
             element = next(iterator, None)
             if element is None:
                 break
-            print(element, end=" ")
+            print(element, end="")
             count += 1
 
     except TypeError:
