53d0226ba160eb957ec9318a21909c1699411cfd
Jonah <jonahemmanuel54@gmail.com>
2023-06-26 04:26:15 -0700

    safe print list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+
+    try:
+        for element in my_list:
+            if count < x:
+                print(element, end=" ")
+                count += 1
+            else:
+                break
+
+    except TypeError:
+        print("Error: Invalid list!")
+
+    print()
+
+    return count
