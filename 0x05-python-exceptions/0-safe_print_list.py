9ae041bcba2a094e8af2b9bc2ed5bc57101344f2
Jonah <jonahemmanuel54@gmail.com>
2023-06-26 05:03:09 -0700

    fix on task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,12 +3,14 @@ def safe_print_list(my_list=[], x=0):
     count = 0
 
     try:
-        for element in my_list:
-            if count < x:
-                print(element, end=" ")
-                count += 1
-            else:
+        iterator = iter(my_list)
+
+        while count < x:
+            element = next(iterator, None)
+            if element is None:
                 break
+            print(element, end=" ")
+            count += 1
 
     except TypeError:
         print("Error: Invalid list!")
