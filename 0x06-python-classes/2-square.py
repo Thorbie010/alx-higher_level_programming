cb75781e6d68eacc11704da2ac966ddba0c65f52
Thorbie010 <125548822+Thorbie010@users.noreply.github.com>
2023-06-28 04:06:34 +0100

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,6 +5,7 @@ Created on Tue June 28 03:53 2023
 @author: Jonah Emmanuel
 """
 
+
 class Square:
     """Class Square that has attributes. Instantiation with size
 
@@ -27,3 +28,4 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+            
