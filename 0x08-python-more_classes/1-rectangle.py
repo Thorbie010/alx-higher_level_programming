7c66c5e460a044e7be6f4418340a9fe7af83f7ee
Jonah <jonahemmanuel54@gmail.com>
2023-07-03 09:40:48 -0700

    fix task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -49,7 +49,7 @@ class Rectangle:
             TypeError: If height is not an integer
             ValueError: If height is less than 0
         """
-        if not isinstance(value, int) :
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
