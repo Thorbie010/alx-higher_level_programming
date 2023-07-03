35528651545e8a6d869c06e14962dbc09ecd4112
Jonah <jonahemmanuel54@gmail.com>
2023-07-03 10:31:22 -0700

    fixing task 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -24,8 +24,8 @@ class Rectangle:
         self.width = width
         self.height = height
         """
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def height(self):
