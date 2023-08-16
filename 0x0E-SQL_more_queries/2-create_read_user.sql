eb6acef9cc23151c9c36fcf545ff2dd9bc8c22ab
Jonah <jonahemmanuel54@gmail.com>
2023-08-16 12:16:36 -0700

    creating a database and user with only select privilede in the database created

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- creatin a database and a new user for the database
+-- the user will have only select privleges in the database created
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
