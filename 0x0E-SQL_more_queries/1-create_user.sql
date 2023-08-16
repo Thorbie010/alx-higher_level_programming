43cca5ae18bd4e674da24a6b08950c87a112e00d
Jonah <jonahemmanuel54@gmail.com>
2023-08-16 12:05:26 -0700

    creating new user on server

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- creating new user on MySQL server
+-- - using CREATE USER command to create new user and using GRANT ALL to grant all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
