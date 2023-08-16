4931a1848f32db807bd689b6e8af96f1b58ad200
Jonah <jonahemmanuel54@gmail.com>
2023-08-16 13:19:34 -0700

    task 5, 6, 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- Create database and table
+-- DDL query 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- set db to new db
+USE hbtn_0d_usa;
+-- create new table
+CREATE TABLE IF NOT EXISTS cities(
+id INT NOT NULL AUTO_INCREMENT UNIQUE,
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL, PRIMARY KEY (id)
+FOREIGN KEY(state_id) REFERENCES stste(id)
+);
