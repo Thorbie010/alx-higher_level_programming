ce7725a0167eb41e5cc4a38e1df69066699c2d26
Jonah <jonahemmanuel54@gmail.com>
2023-08-16 13:46:35 -0700

    fixed syntax error in task 7

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,12 +1,10 @@
 -- Create database and table
 -- DDL query 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
--- set db to new db
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
 USE hbtn_0d_usa;
--- create new table
 CREATE TABLE IF NOT EXISTS cities(
 id INT NOT NULL AUTO_INCREMENT UNIQUE,
 state_id INT NOT NULL,
 name VARCHAR(256) NOT NULL, PRIMARY KEY (id),
-FOREIGN KEY(state_id), REFERENCES states(id)
+FOREIGN KEY(state_id) REFERENCES states(id)
 );
