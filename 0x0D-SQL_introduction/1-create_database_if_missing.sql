2d34660b4de81b71c1d3c8eda4073956be86a215
Jonah <jonahemmanuel54@gmail.com>
2023-08-15 18:46:37 -0700

    task 1-15

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- creates a table in sql
+-- create hbtn_0c_0 table in mySQL server
+CREATE DATABASE IF NOT EXIST hbtn_0c_0;vv
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Deletes a database in sql
+-- Deletes hbtn_0c_0 database from mySQL server
+DROP DATABASE IF EXIST hbtn_0c_0;
