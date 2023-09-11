12f835097a9f29bc0023c0d6121ffe1450adaaed
thorbie <jonahemmanuel54@gmail.com>
2023-09-11 15:45:41 +0100

    fixing errors for taskk 7, 9 and 12

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-const times = parseInt(process.argv[2]);
-
-if (!isNaN(times)) {
-  let output = '';
-  for (let i = 0; i < times; i++) {
-    output += 'C is fun\n';
+if (process.argv[2]) {
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log('C is fun\n');
   }
-  console.log(output);
 } else {
   console.log('Missing number of occurences');
 }
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -4,8 +4,5 @@ function add (a, b) {
 }
 const num1 = parseInt(process.argv[2]);
 const num2 = parseInt(process.argv[3]);
-
-if (!isNaN(num1) && (num2)) {
-  const result = add(num1, num2);
-  console.log(result);
-}
+const result = add(num1, num2);
+console.log(result);
