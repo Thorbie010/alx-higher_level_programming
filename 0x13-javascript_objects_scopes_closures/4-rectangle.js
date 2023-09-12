fcfc2325ccdaa498d71d74c6411eacfd88768181
Jonah Emmanuel <Jonahemmanuel54@gmail.com>
2023-09-12 06:15:03 -0700

    fixing semistandard for task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -9,16 +9,14 @@ class Rectangle {
 
   print () {
     for (let row = 0; row < this.height; row++) {
-        console.log('x'.repeat(this.width));
-      }
+      console.log('x'.repeat(this.width));
     }
-
+  }
 
   double () {
     this.width *= 2;
     this.height *= 2;
   }
-  
 
   rotate () {
     const temp = this.width;
