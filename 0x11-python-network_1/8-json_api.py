f764392bba0e730080dd784671010d4fc98421a0
Jonah <jonahemmanuel54@gmail.com>
2023-09-29 17:38:14 -0700

    fixing pycodestyle error

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -22,5 +22,5 @@ if __name__ == "__main__":
             print('No result')
         else:
             print('[{}] {}'.format(obj['id'], obj['name']))
-    except:
+    except ValueError:
         print('Not a valid JSON')
